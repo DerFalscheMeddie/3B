233f427c3800f89ad4c2d6f64247d431ed374e30
Marvin Reufels <marvin.reufels@mni.thm.de>
2026-05-07 18:00:27 +0200

    init

---
+++ level.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+
+
+var childs
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	childs = get_children()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	for child in childs:
+		if (is_instance_of(child, Area2D)):
+			child.rotate(0.05)
+	
+	if (Input.is_action_just_pressed("Input")):
+		if (Input.is_key_pressed(KEY_Q)):
+			childs[0].modulate = Color(randf(), randf(), randf())
+		if (Input.is_key_pressed(KEY_W)):
+			childs[1].modulate = Color(randf(), randf(), randf())
+		if (Input.is_key_pressed(KEY_E)):
+			childs[2].modulate = Color(randf(), randf(), randf())
